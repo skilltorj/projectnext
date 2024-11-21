8dc50f65ed6df4301642ff31fb61f70354c1cef6
skilltorj <skilltorj@gmail.com>
2024-11-21 18:38:38 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,36 @@ module.exports = {
       fontFamily: {
         sans: ['poppins', 'sans-serif'],
       },
+      screens: {
+        xl: '1439px', // Adjust as per your need
+        'xs': '375px', // Custom breakpoint for 375px
+        'sm-xs': '425px', // Custom breakpoint for 420px
+        'xs-lg': {min: '1440px', max:'1496px'},
+        'custom-change':{min:'1496px', max:'1536px'},
+        'range-custom': {min: '640px', max:'768px'},
+        'range': {min: '440px', max:'500px'},
+        'custom': {min: '500px', max:'510px'},
+        'custom-md': {min:'510px', max:'520px'},
+        'custom-lg':{min:'520px', max: '530px'},
+        'custom-sm':{min:'530px', max:'540px'},
+        'custom-xl':{min:'540px', max:'550px'},
+        'range-sm':{min:'550px', max:'560px'},
+        'range-md':{min: '560px', max:'570px'},
+        'range-lg':{min:'570px', max:'580px'},
+        'range-xl': {min:'580px', max:'590px'},
+        'range-xs-sm':{min:'590px', max:'600px'},
+        'custom-xs':{min: '600px', max: '630px'},
+        'range-xs': {min: '630px', max:'640px'},
+        'md-xs': {min: '768px', max:'778px'},
+        'ms-md':{min: '778px', max:'800px'},
+        'md-ms':{min:'800px', max:'900px'},
+        'sm-md':{min:'900px', max:'1000px'},
+        'lg-md':{min:'1000px', max:'1024px'},
+        'lg-sm':{min:'1024px', max:'1440px'},
+        'md-lg':{min:'320px', max:'375px'},
+        'xss':'1024px',
+        'ls':'1440px',
+      },
 
       brightness: {
         70: '.70',
